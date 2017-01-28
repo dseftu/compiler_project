74e4da59336b39782355e18719ea731470685893
David Allen <David Allen>
2017-01-28 12:03:19 -0500

    noon update

--- P-machine.c
+++ P-machine.c
@@ -13,7 +13,9 @@
 // PC, the program counter
 // IR, the instruciton register
 
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #define MAX_STACK_HEIGHT 2000
 #define MAX_CODE_LENGTH 500
@@ -24,17 +26,75 @@
 #define RTN 2
 #define LOD 3
 #define STO 4
+#define CAL 5
+#define INC 6
+#define JMP 7
+#define JPC 8
+
+// techincally these are all "SIO", but I added
+// clarity to the differences.  SIO_O displays
+// output to screen, SIO_I gets input, and
+// SIO_E ends the program
+#define SIO_O 9 
+#define SIO_I 10
+#define SIO_E 11
+
+// Arithmatic and logical instructions
+// these all do a task with 1 or more registers
+// and put the result in a register
+#define NEG 12
+#define ADD 13
+#define SUB 14
+#define MUL 15
+#define DIV 16
+#define ODD 17
+#define MOD 18
+#define EQL 19
+#define NEQ 20
+#define LSS 21
+#define LEQ 22
+#define GTR 23
+#define GEQ 24
+
+typedef struct instruction
+{
+    int op; // opcode
+    int r; // reg
+    int l; // L
+    int m; // M
+} instruction;
+
+// set up the four registers to handle the stack/code
+int BP = 1;
+int SP = 0;
+int PC = 0;
+int IR = 0;
+
+int instruction[MAX_CODE_LENGTH];
+int stack[MAX_STACK_HEIGHT];
 
 int main()
 {
-    // set up the four registers to handle the stack/code
-    int BP = 1;
-    int SP = 0;
-    int PC = 0;
-    int IR = 0;
-
-    int code[MAX_CODE_LENGTH];
-    int stack[MAX_STACK_HEIGHT];
     return 0;
+}
+
+void readInput(string filename)
+{
+
+}
+
+// Code provided by the problem statement:
+// Find base L levels down
+int base(l, base)
+{
+    int b1;
+    b1 = base;
+
+    while (l > 0)
+    {
+        b1 = stack[b1 +1];
+        l--;
+    }
+    return b1;
 }
 
