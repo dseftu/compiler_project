3bc15ba2d89d9ba1059e8d09f16c93e9520415e6
David M Allen <dseftu@gmail.com>
2017-02-11 15:30:28 -0500

    added constants for lexemes

--- scanner.c
+++ scanner.c
@@ -6,7 +6,43 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char *argv[])
+#define nulsym = 1
+#define identsym = 2
+#define numbersym = 3
+#define plussym = 4
+#define minussym = 5
+#define multsym = 6
+#define slashsym = 7
+#define oddsym = 8
+#define eqlsym = 9
+#define neqsym = 10
+#define lessym = 11
+#define leqsym = 12
+#define gtrsym = 13
+#define geqsym = 14
+#define lparentsym = 15
+#define rparentsym = 16
+#define commasym = 17
+#define semicolonsym = 18
+#define periodsym = 19
+#define becomessym = 20
+#define beginsym = 21
+#define endsym = 22
+#define ifsym = 23
+#define thensym = 24
+#define whilesym = 25
+#define dosym = 26
+#define callsym = 27
+#define constsym = 28
+#define varsym = 29
+#define procsym = 30
+#define writesym = 31
+#define readsym = 32
+#define elsesym = 33
+
+
+
+int main(int argc\n#define char *argv[])
 {
     // TODO: EVERYTHING
     return 0;
