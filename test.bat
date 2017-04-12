208625d8bd2e246c0c14ea2e01321aa27a78b33c
David M Allen <dseftu@gmail.com>
2017-04-11 20:43:47 -0400

    fact works again

--- test.bat
+++ test.bat
@@ -4,7 +4,7 @@ echo Compiler for COP 3402 - Systems Software
 echo Authored by Dallas Seroski and David M. Allen
 echo Spring 2017
 echo =============================================
-echo Sample Program (test2.pl0)
+echo test2 Program (test2.pl0)
 echo.
 echo.
 type .\GoodPL0Files\test2.pl0
@@ -12,4 +12,4 @@ echo.
 echo.
 echo .\bin\compiler.exe .\GoodPL0Files\test2.pl0
 echo.
-.\bin\compiler.exe -a -v .\GoodPL0Files\test2.pl0
+.\bin\compiler.exe -a .\GoodPL0Files\test2.pl0
